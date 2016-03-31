473e0c40b37c54a32aa3520f9525e12056914687
Ruhul Amin <mruhul@gmail.com>
2016-04-01 00:23:44 +1100

    Initial Commit

---
+++ Src/Sample.Api/Ioc/ConventionBasedModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+
+namespace Sample.Api.Ioc
+{
+    public class ConventionBasedModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var assemblies = new[]
+            {typeof (Startup).Assembly, typeof (Sample.Infrastructure.PersistentStores.IPersistentStore).Assembly};
+
+            builder.RegisterAssemblyTypes(assemblies)
+                .Where(a => a.IsClass && 
+                    (a.Name.EndsWith("Handler") 
+                    || a.Name.EndsWith("Filter")
+                    || a.Name.EndsWith("Proxy")
+                    || a.Name.EndsWith("Store")))
+                .AsImplementedInterfaces();
+        }
+    }
+}
