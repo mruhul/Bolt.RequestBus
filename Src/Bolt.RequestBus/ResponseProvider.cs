6756ff3217edde1e740c3bd9b6ecf24fe39caaf4
Mohammad Ruhul Amin <mohammadruhul.amin@carsales.office>
2018-08-10 21:28:27 +1000

    wip

--- Src/Bolt.RequestBus/ResponseProvider.cs
+++ Src/Bolt.RequestBus/ResponseProvider.cs
@@ -29,139 +29,15 @@ namespace Bolt.RequestBus
 
         public async Task<IEnumerable<IResponseUnit<TResult>>> GetAllAsync<TResult>()
         {
-            var result = new List<IResponseUnit<TResult>>();
-
             var context = await _serviceProvider.BuildContextAsync();
-
-            var allApplicableHandlers = _serviceProvider.GetServices<IResponseHandlerAsync<TResult>>()
-                               ?.Where(t => t.IsApplicable(context)) 
-                               ?? Enumerable.Empty<IResponseHandlerAsync<TResult>>();
-
-            var mainHandlers = allApplicableHandlers.Where(t => t.ExecutionHint == ExecutionHintType.Independent 
-                                                    || t.ExecutionHint == ExecutionHintType.Main);
-
-            var mainHandlerTasks = mainHandlers
-                                        .Select(h => h.Handle(context)
-                                                    .ContinueWith(t => Convert(t.Result, h.ExecutionHint)));
-
-            await Task.WhenAll(mainHandlerTasks);
-
-            var isMainFailed = false;
-
-            foreach(var task in mainHandlerTasks)
-            {
-                if (task.Result == null) continue;
-
-                if (task.Result.IsMainResponse)
-                {
-                    isMainFailed = task.Result.IsSucceed;
-                }
-
-                result.Add(task.Result);
-            }
-
-            if (isMainFailed) return result;
-
-            var dependentHandlers = allApplicableHandlers.Where(h => h.ExecutionHint == ExecutionHintType.Dependent);
-
-            var dependentHandlerTasks = dependentHandlers
-                                        .Select(h => h.Handle(context)
-                                                    .ContinueWith(t => Convert(t.Result, h.ExecutionHint)));
-
-            await Task.WhenAll(dependentHandlerTasks);
-
-            foreach(var task in dependentHandlerTasks)
-            {
-                if (task.Result == null) continue;
-
-                result.Add(task.Result);
-            }
-
-            var filters = _serviceProvider.GetServices<IResponseCollectionFilterAsync<TResult>>()
-                            ?.Where(x => x.IsApplicable(context)) 
-                            ?? Enumerable.Empty<IResponseCollectionFilterAsync<TResult>>();
-
-            foreach(var filter in filters)
-            {
-                await filter.Filter(context, result);
-            }
-
-            return result;
+            return await ResponseProviderBus.GetAllAsync<TResult>(_serviceProvider, context, _logger);
         }
 
         public async Task<IEnumerable<IResponseUnit<TResult>>> GetAllAsync<TRequest, TResult>(TRequest request)
         {
             var context = await _serviceProvider.BuildContextAsync();
 
-            var errors = await _serviceProvider.ValidateAsync(context, request, _logger);
-
-            var result = new List<IResponseUnit<TResult>>();
-
-            if (errors.Any())
-            {
-                result.Add(new ResponseUnit<TResult> {
-                    Errors = errors,
-                    IsMainResponse = true,
-                    IsSucceed = false
-                });
-
-                return result;
-            }
-
-            var allApplicableHandlers = _serviceProvider.GetServices<IResponseHandlerAsync<TRequest,TResult>>()
-                               ?.Where(t => t.IsApplicable(context, request))
-                               ?? Enumerable.Empty<IResponseHandlerAsync<TRequest,TResult>>();
-
-            var mainHandlers = allApplicableHandlers.Where(t => t.ExecutionHint == ExecutionHintType.Independent
-                                                    || t.ExecutionHint == ExecutionHintType.Main);
-
-            var mainHandlerTasks = mainHandlers
-                                        .Select(h => h.Handle(context, request)
-                                                    .ContinueWith(t => Convert(t.Result, h.ExecutionHint)));
-
-            await Task.WhenAll(mainHandlerTasks);
-
-            var isMainFailed = false;
-
-            foreach (var task in mainHandlerTasks)
-            {
-                if (task.Result == null) continue;
-
-                if (task.Result.IsMainResponse)
-                {
-                    isMainFailed = task.Result.IsSucceed;
-                }
-
-                result.Add(task.Result);
-            }
-
-            if (isMainFailed) return result;
-
-            var dependentHandlers = allApplicableHandlers.Where(h => h.ExecutionHint == ExecutionHintType.Dependent);
-
-            var dependentHandlerTasks = dependentHandlers
-                                        .Select(h => h.Handle(context, request)
-                                                    .ContinueWith(t => Convert(t.Result, h.ExecutionHint)));
-
-            await Task.WhenAll(dependentHandlerTasks);
-
-            foreach (var task in dependentHandlerTasks)
-            {
-                if (task.Result == null) continue;
-
-                result.Add(task.Result);
-            }
-
-            var filters = _serviceProvider.GetServices<IResponseCollectionFilterAsync<TRequest,TResult>>()
-                            ?.Where(x => x.IsApplicable(context, request))
-                            ?? Enumerable.Empty<IResponseCollectionFilterAsync<TRequest,TResult>>();
-
-            foreach (var filter in filters)
-            {
-                await filter.Filter(context, request, result);
-            }
-
-            return result;
+            return await ResponseProviderBus.GetAllAsync<TRequest, TResult>(_serviceProvider, context, _logger, request);
         }
 
         public async Task<TResult> GetAsync<TResult>()
@@ -175,35 +51,7 @@ namespace Bolt.RequestBus
         {
             var context = await _serviceProvider.BuildContextAsync();
 
-            var errors = await _serviceProvider.ValidateAsync(context, request, _logger);
-
-            if (errors.Any())
-            {
-                return Response.Failed<TResult>(errors);
-            }
-
-            var handler = _serviceProvider.GetServices<IResponseHandlerAsync<TRequest,TResult>>()
-                            ?.Where(h => h.IsApplicable(context, request)).FirstOrDefault();
-
-            if (handler == null) throw new Exception($"No response handler defined for type {typeof(IResponseHandlerAsync<TRequest,TResult>)}");
-
-            var response = await handler.Handle(context, request);
-
-            if (response == null) return null;
-
-            var filters = _serviceProvider.GetServices<IResponseFilterAsync<TRequest, TResult>>()
-                                ?.Where(h => h.IsApplicable(context, request))
-                                ?? Enumerable.Empty<IResponseFilterAsync<TRequest, TResult>>();
-
-            if(filters == null)
-            {
-                foreach(var filter in filters)
-                {
-                    await filter.Filter(context, request, response);
-                }
-            }
-
-            return response;
+            return await ResponseProviderBus.GetAsync<TRequest, TResult>(_serviceProvider, context, _logger, request, failSafe: false);
         }
 
 
--- Src/Bolt.RequestBus/ResponseProviderBus.cs
+++ Src/Bolt.RequestBus/ResponseProviderBus.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -55,7 +56,7 @@ namespace Bolt.RequestBus
 
             if (handler == null)
             {
-                if(failSafe)
+                if (failSafe)
                 {
                     logger.LogWarning($"No handler found that impement IResponseProvider<{typeof(TResult).FullName}>");
 
@@ -81,6 +82,208 @@ namespace Bolt.RequestBus
             await sp.FilterAsync(context, response, logger);
 
             return response.Result;
+        }
+
+        public static async Task<IEnumerable<IResponseUnit<TResult>>> GetAllAsync<TRequest, TResult>(IServiceProvider sp, IExecutionContext context, ILogger logger, TRequest request)
+        {
+            var errors = await sp.ValidateAsync(context, request, logger);
+
+            var result = new List<IResponseUnit<TResult>>();
+
+            if (errors.Any())
+            {
+                result.Add(new ResponseUnit<TResult>
+                {
+                    Errors = errors,
+                    IsMainResponse = true,
+                    IsSucceed = false
+                });
+
+                return result;
+            }
+
+            var allApplicableHandlers = sp.GetServices<IResponseHandlerAsync<TRequest, TResult>>()
+                               ?.Where(t => t.IsApplicable(context, request))
+                               ?? Enumerable.Empty<IResponseHandlerAsync<TRequest, TResult>>();
+
+            var mainHandlers = allApplicableHandlers.Where(t => t.ExecutionHint == ExecutionHintType.Independent
+                                                    || t.ExecutionHint == ExecutionHintType.Main);
+
+            var mainHandlerTasks = mainHandlers
+                                        .Select(h => HandleResponseHandler(context, h, request, logger));
+
+            await Task.WhenAll(mainHandlerTasks);
+
+            var isMainFailed = false;
+
+            foreach (var task in mainHandlerTasks)
+            {
+                if (task.Result == null) continue;
+
+                if (task.Result.IsMainResponse)
+                {
+                    isMainFailed = task.Result.IsSucceed;
+                }
+
+                result.Add(task.Result);
+            }
+
+            if (isMainFailed) return result;
+
+            var dependentHandlers = allApplicableHandlers.Where(h => h.ExecutionHint == ExecutionHintType.Dependent);
+
+            var dependentHandlerTasks = dependentHandlers
+                                        .Select(h => HandleResponseHandler(context, h, request, logger));
+
+            await Task.WhenAll(dependentHandlerTasks);
+
+            foreach (var task in dependentHandlerTasks)
+            {
+                if (task.Result == null) continue;
+
+                result.Add(task.Result);
+            }
+
+            var filters = sp.GetServices<IResponseCollectionFilterAsync<TRequest, TResult>>()
+                            ?.Where(x => x.IsApplicable(context, request))
+                            ?? Enumerable.Empty<IResponseCollectionFilterAsync<TRequest, TResult>>();
+
+            foreach (var filter in filters)
+            {
+#if DEBUG
+                var timer = Timer.Start(logger, filter);
+#endif
+                await filter.Filter(context, request, result);
+#if DEBUG
+                timer.Completed();
+#endif
+            }
+
+            return result;
+        }
+
+        public static async Task<IEnumerable<IResponseUnit<TResult>>> GetAllAsync<TResult>(IServiceProvider sp, IExecutionContext context, ILogger logger)
+        {
+            var result = new List<IResponseUnit<TResult>>();
+
+            var allApplicableHandlers = sp.GetServices<IResponseHandlerAsync<TResult>>()
+                               ?.Where(t => t.IsApplicable(context))
+                               ?? Enumerable.Empty<IResponseHandlerAsync<TResult>>();
+
+            var mainHandlers = allApplicableHandlers.Where(t => t.ExecutionHint == ExecutionHintType.Independent
+                                                    || t.ExecutionHint == ExecutionHintType.Main);
+
+            var mainHandlerTasks = mainHandlers
+                                        .Select(h => HandleResponseHandler(h, context, logger));
+
+            await Task.WhenAll(mainHandlerTasks);
+
+            var isMainFailed = false;
+
+            foreach (var task in mainHandlerTasks)
+            {
+                if (task.Result == null) continue;
+
+                if (task.Result.IsMainResponse)
+                {
+                    isMainFailed = task.Result.IsSucceed;
+                }
+
+                result.Add(task.Result);
+            }
+
+            if (isMainFailed) return result;
+
+            var dependentHandlers = allApplicableHandlers.Where(h => h.ExecutionHint == ExecutionHintType.Dependent);
+
+            var dependentHandlerTasks = dependentHandlers
+                                        .Select(h => HandleResponseHandler(h, context, logger));
+
+            await Task.WhenAll(dependentHandlerTasks);
+
+            foreach (var task in dependentHandlerTasks)
+            {
+                if (task.Result == null) continue;
+
+                result.Add(task.Result);
+            }
+
+            var filters = sp.GetServices<IResponseCollectionFilterAsync<TResult>>()
+                            ?.Where(x => x.IsApplicable(context))
+                            ?? Enumerable.Empty<IResponseCollectionFilterAsync<TResult>>();
+
+            foreach (var filter in filters)
+            {
+#if DEBUG
+                var timer = Timer.Start(logger, filter);
+#endif
+                await filter.Filter(context, result);
+#if DEBUG
+                timer.Completed();
+#endif
+            }
+
+            return result;
+        }
+
+        private static async Task<IResponseUnit<TResult>> HandleResponseHandler<TResult>(IResponseHandlerAsync<TResult> h, IExecutionContext context, ILogger logger)
+        {
+            IResponse<TResult> rsp;
+#if DEBUG
+            var timer = Timer.Start(logger, h);
+#endif
+            if (h.ExecutionHint == ExecutionHintType.Main)
+            {
+                rsp = await h.Handle(context);
+            }
+            else
+            {
+                try
+                {
+                    rsp = await h.Handle(context);
+                }
+                catch (Exception e)
+                {
+                    rsp = Response.Failed<TResult>();
+                    logger.LogError(e, e.Message);
+                }
+            }
+
+#if DEBUG
+            timer.Completed();
+#endif
+
+            return Convert(rsp, h.ExecutionHint);
+        }
+
+        private static async Task<IResponseUnit<TResult>> HandleResponseHandler<TRequest, TResult>(IExecutionContext context, IResponseHandlerAsync<TRequest, TResult> h, TRequest request, ILogger logger)
+        {
+
+#if DEBUG
+            var timer = Timer.Start(logger, h);
+#endif
+            IResponse<TResult> rsp;
+            if (h.ExecutionHint != ExecutionHintType.Main)
+            {
+                rsp = await h.Handle(context, request);
+            }
+            else
+            {
+                try
+                {
+                    rsp = await h.Handle(context, request);
+                }
+                catch (Exception e)
+                {
+                    rsp = Response.Failed<TResult>();
+
+                    logger.LogError(e, $"ResponseHandler {h.GetType().FullName} failed with message {e.Message}");
+                }
+            }
+#if DEBUG
+            timer.Completed();
+#endif
+            return Convert(rsp, h.ExecutionHint);
         }
 
         private static ResponseUnit<TResult> Convert<TResult>(IResponse<TResult> source, ExecutionHintType hintType)
